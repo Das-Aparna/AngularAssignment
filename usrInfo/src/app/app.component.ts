92c985015cef7a15e4986029ef39fb036b062244
Das-Aparna <88975860+Das-Aparna@users.noreply.github.com>
2023-02-27 21:52:10 +0530

    update style

--- usrInfo/src/app/app.component.ts
+++ usrInfo/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT, DatePipe } from '@angular/common';
-import { observable, Observable } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +19,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.document.querySelector('#date').textContent = this.datePipe.transform(
       this.myDate,
-      'yy-MMMM-dd'
+      'dd-MMMM-yy'
     );
   }
 }
